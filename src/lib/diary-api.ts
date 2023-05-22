46c6cf92c3ec0ff2545e963816cd91702d52e266
iori0705 <iodere77@gmail.com>
2023-05-22 22:36:33 +0900

    prettier

--- src/lib/diary-api.ts
+++ src/lib/diary-api.ts
@@ -1,7 +1,6 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
-import { glob, globSync, globStream, globStreamSync, Glob } from "glob";
 
 type Post = {
   slug: string;
